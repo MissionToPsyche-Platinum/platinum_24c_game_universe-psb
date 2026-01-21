57cfbd0bb7e7eb2be130f4d73032ee587231b5d9
mam9265 <mam9265@psu.edu>
2026-01-21 11:12:08 -0500

    Added "Mini Psyche" which is what the player controls during minigames
    Its a mini version of Psyche!

--- Model/Scenes/Obstacle.gd
+++ Model/Scenes/Obstacle.gd
@@ -7,3 +7,10 @@ func _process(delta):
 	
 	if position.y > 700:
 		queue_free()
+		
+func _ready():
+	body_entered.connect(on_body_entered)
+
+func on_body_entered(body):
+	if body.name == "Player":
+		get_tree().reload_current_scene()
