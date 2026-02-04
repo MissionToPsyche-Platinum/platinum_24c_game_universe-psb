112ca575456448a06ab7a45299e4f9c4dbef597f
konradRuck <konradruck@gmail.com>
2026-02-04 02:29:07 -0500

    Created map resource and model
    Created MapLayout resource and MapModel. Added Map_9Nodes.tres (same nodes and coordinates currently hardcoded in map.gd).

---
+++ Model/MapData/MapModel.gd
@@ -0,0 +1,43 @@
+extends Object
+class_name MapModel
+# MapModel class is used as a part of the map's MVC structure
+# Initialzed based on a particular MapLayout
+
+signal changed
+signal reached_end
+
+var layout: MapLayout
+
+var current_index := -1
+var known_nodes := {}
+var disabled_nodes := {}
+
+# Initialize layout, current index & earth node
+func _init(map_layout: MapLayout):
+	layout = map_layout
+	current_index = layout.start_index
+	known_nodes[current_index] = true
+
+# Helper to get all proceeding neighbors of index
+func get_proceeding_neighbors(index: int) -> Array[int]:
+	var neighbors := []
+	for c in layout.connections:
+		if c.x == index:
+			neighbors.append(c.y)
+	return neighbors
+
+# Helper to determine viable moves from index
+func can_move_to(index: int) -> bool:
+	return get_proceeding_neighbors(current_index).has(index)
+
+# Changes current_index to index only if it is a neighbor
+func move_to(index: int):
+	if !can_move_to(index):
+		return
+
+	current_index = index
+	known_nodes[index] = true
+	changed.emit() # Index has changed (Psyche moves)
+
+	if index == layout.end_index:
+		reached_end.emit() # Asteroid has been reached
